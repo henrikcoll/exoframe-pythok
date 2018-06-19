c5298058a5376e488aacfa82469641c809a53771
Henrik Næss <henrik@heka.no>
2018-06-20 01:40:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const path = require('path');
 
 const pythonDockerfile = () =>
   `FROM python:3
+
 WORKDIR /usr/src/app
+
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
+
 COPY . .
+
+EXPOSE 80
+
 CMD [ "python", "./main.py" ]`;
 
 exports.name = 'python';
